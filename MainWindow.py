7b89ffa2917c757645f65957953f3e99df46153d
Insane Shadow <insaneshadowzaman@gmail.com>
2018-08-03 02:40:39 +0600

    Added Video, Audio and news support

--- MainWindow.py
+++ MainWindow.py
@@ -9,7 +9,6 @@ from PyQt5.uic import loadUi
 
 from Speach import Speach
 from image_processors.BlinkDetector import BlinkDetector
-from image_processors.GazeDetector import GazeDetector
 from image_processors.OnlyGazeShower import OnlyGazeShower
 
 
@@ -18,8 +17,6 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         loadUi("./ui/MainWindow.ui", self)
         self.setWindowTitle("Eye Gaze Detection")
-        self.upload_haar_button.clicked.connect(self.selectHaar)
-        self.haar_file_location_lineEdit.setText("haar_cascades/cascade.xml")
 
         # mode 0 = not controlling wheel chair; controlling menu with eye-blink
         # mode 1 = controling wheel chair with eye-gaze
@@ -37,8 +34,8 @@ class MainWindow(QMainWindow):
         self.blinkDetector.rightAddCallback(self.moveFocusRight)
         self.blinkDetector.bothAddCallback(self.pressFocused)
 
-        # self.speech = Speach()
-        # self.speechObj()
+        self.speech = Speach()
+        self.speechObj()
 
         self.currentFocus = 0
         self.__initialize_buttons()
@@ -71,24 +68,84 @@ class MainWindow(QMainWindow):
                 self.thread.start()
                 print("thread started")
 
+    def setCurrentMode(self, i):
+        self.current_mode = i
+
     def __initialize_buttons(self):
+        self.selectMethodComboBox.clear()
+        self.selectMethodComboBox.addItems([
+            "Eye-Help",
+            "Head-Help",
+            "Voice-Help"
+        ])
+        self.selectMethodComboBox.setCurrentIndex(0)
+        self.selectMethodComboBox.currentIndexChanged.connect(lambda: self.setCurrentMode(self.selectMethodComboBox.currentIndex()))
+
+
+
         self.buttons = [self.b1_1, self.b1_2, self.b1_3,
                         self.b2_1, self.b2_2, self.b2_3,
                         self.b3_1, self.b3_2, self.b3_3]
         for b in self.buttons:
             b.setAutoDefault(True)
         self.buttons[self.currentFocus].setFocus(True)
-        self.b2_2.clicked.connect(self.toggleEyeGaze)
-        self.b3_3.clicked.connect(self.soundModeOn)
 
-    def toggleEyeGaze(self):
+        self.b1_1.clicked.connect(self.controlWheel)
+        self.b1_2.clicked.connect(self.playSMS)
+        self.b1_3.clicked.connect(self.playEmail)
+        self.b2_1.clicked.connect(self.playVideo)
+        self.b2_2.clicked.connect(self.playMusic)
+        self.b2_3.clicked.connect(self.playBrowser)
+        self.b3_1.clicked.connect(self.playLight)
+        self.b3_2.clicked.connect(self.playFan)
+        self.b3_3.clicked.connect(self.playEmergency)
+
+    def playEmail(self):
+        pass
+
+    def playSMS(self):
+        pass
+
+    def controlWheel(self):
+        pass
+
+    def playEmergency(self):
+        # TODO: socket programming
+        pass
+
+    def playFan(self):
+        # TODO: socket programming
+        pass
+
+    def playLight(self):
+        # TODO: socket programming
+        pass
+
+    def playBrowser(self):
+        import webbrowser
+        webbrowser.open_new_tab("http://epaperna.prothomalo.com/")
+
+    def playMusic(self):
+        import os, random, subprocess
+        randomfile = random.choice(os.listdir("/home/insaneshadow/Music"))
+        file = "/home/insaneshadow/Music/" + randomfile
+        subprocess.call(['deepin-music', file])
+
+    def playVideo(self):
+        import os, random, subprocess
+        randomfile = random.choice(os.listdir("/home/insaneshadow/Videos"))
+        file = "/home/insaneshadow/Videos/" + randomfile
+        subprocess.call(['vlc', file])
+
+
+    def blinkModeOn(self):
+        self.current_mode = 0
+
+    def eyegazeModeOn(self):
         self.current_mode = 1
 
     def soundModeOn(self):
         self.current_mode = 2
-
-    def blinkModeOn(self):
-        self.current_mode = 0
 
     def moveFocusRight(self):
         self.currentFocus = (self.currentFocus + 1) % 9
